af229ed85e5e7443e2bc26f8bc2c1f66926995e8
fromitive <fromitive@gmail.com>
2025-01-07 18:55:57 +0900

    feat: maximum-average-subarray-i 추가 풀이

--- fromitive/maximum-average-subarray-i.py
+++ fromitive/maximum-average-subarray-i.py
@@ -10,6 +10,8 @@ description
 O(n)으로 풀기 위해 sliding-window를 쓸 수 있다.
 k 개수가 고정되어 있고 window size가 k 보다 커지면 left를 움직인다.
 answer에는 nums에 음수값이 포함되어 있으니 최소 입력값을 넣는다.
+
+k 개수가 고정되어 있으니, 유동적인 sliding-window 기법을 사용하지 않아도 풀 수 있다.
 """
 
 class Solution:
@@ -24,4 +26,18 @@ class Solution:
                 left += 1
             if right - left == k - 1:
                 answer = max(answer, totalSum / k)
+        return answer
+
+    def findMaxAverageII(self, nums: List[int], k: int) -> float:
+        sumTable = [0] * len(nums) + 1
+        totalSum = 0
+        for index, num in enumerate(nums):
+            totalSum += num
+            sumTable[index + 1] = totalSum
+        answer = sumTable[k] / k # k <= len(nums)
+
+        for index in range(k, len(nums) + 1):
+            average = (sumTable[index] - sumTable[index - k]) / k
+            answer = max(ansewr, average)
+        
         return answer

07085995b19f9f8cdd2e666e531c5dc5b0fe1ae1
slimsha2dy <chy2581@naver.com>
2025-03-09 01:07:14 +0900

    11-3

---
+++ slimsha2dy/30191.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+
+using namespace std;
+int n;
+vector<char> v;
+stack<int> s;
+
+void recur() {
+	if (s.empty()) return;
+	char tmp = s.top(); s.pop();
+	if (tmp == 'U') {
+		v.push_back('S');
+		v.push_back('N');
+		while (s.top() != 'S')
+			recur();
+		s.pop();
+		v.push_back('N');
+	}
+	else if (tmp == 'S') {
+		v.push_back('U');
+		v.push_back('N');
+		while (s.top() != 'U')
+			recur();
+		s.pop();
+		v.push_back('N');
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	string str;
+	cin >> n;
+	cin >> str;
+
+	for (char c : str) s.push(c);
+	while (!s.empty()) recur();
+	for (char c : v) cout << c;
+}
+
